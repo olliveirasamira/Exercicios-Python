5818abe60ebf1a4f574238fee83c48891ab96977
olliveirasamira <79542385+olliveirasamira@users.noreply.github.com>
2021-05-09 19:18:23 -0300

    Rename Ex012.py to 012.py

---
+++ 012.py
@@ -0,0 +1,3 @@
+preço = float(input('Qual é o preço do produto? R$'))
+novo = preço - (preço*5/100)
+print('O Produto que custava R$ {}, na promoção com desconto de 5%, irá custar R${}' . format(preço, novo))
--- Ex012.py
+++
@@ -1,3 +0,0 @@
-preço = float(input('Qual é o preço do produto? R$'))
-novo = preço - (preço*5/100)
-print('O Produto que custava R$ {}, na promoção com desconto de 5%, irá custar R${}' . format(preço, novo))
